fc7ae0f04871dd2d5e92155ff784f81b96a66ed3
luca <luca02c2@gmail.com>
2024-04-29 13:33:44 +0200

    Create profile screen

--- FoodAndArt/app/src/main/java/com/example/foodandart/ui/screens/profile/ProfileScreen.kt
+++ FoodAndArt/app/src/main/java/com/example/foodandart/ui/screens/profile/ProfileScreen.kt
@@ -1,11 +1,21 @@
 package com.example.foodandart.ui.screens.profile
 
+import android.net.Uri
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ExitToApp
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddLocation
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material.icons.outlined.PersonRemove
 import androidx.compose.material3.AlertDialog
@@ -17,6 +27,8 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MenuItemColors
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -26,14 +38,21 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.example.foodandart.R
 import com.example.foodandart.data.models.Theme
 
@@ -52,28 +71,95 @@ fun ProfileScreen(
 ) {
     LaunchedEffect(Unit) { viewModel.initialize(navController) }
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.app_name)) },
+                actions = {
+                    IconButton(onClick = { expandMenu = true }) {
+                        Icon(Icons.Default.MoreVert, "Menu")
+                    }
+                    Menu(viewModel)
+                }
+            )
+        }
+    ) {contentPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(contentPadding)
+                .onPlaced { it ->
+                    parentWidth = it.size.width
+                },
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
 
-    Column(
+        )
+        {
+            UserInformation(viewModel)
+            SignOutAlert(viewModel)
+            DeleteAccountAlert(viewModel)
+        }
+    }
+}
+
+@Composable
+fun UserInformation(viewModel: ProfileViewModel) {
+    if (viewModel.imageUri != Uri.EMPTY) {
+        val painter = rememberAsyncImagePainter(
+            ImageRequest.Builder(LocalContext.current)
+                .data(data = viewModel.imageUri)
+                .apply(block = fun ImageRequest.Builder.() {
+                    transformations(CircleCropTransformation())
+                }).build()
+        )
+        Image(
+            painter = painter,
+            "Captured image",
+            modifier = Modifier
+                .size(150.dp)
+                .clip(CircleShape),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Fit
+        )
+    } else {
+        Image(
+            Icons.Outlined.AccountCircle,
+            "empty Image",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(150.dp),
+            alignment = Alignment.Center
+        )
+    }
+    OutlinedTextField(
+        singleLine = true,
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight()
-            .onPlaced { it ->
-                parentWidth = it.size.width
-            }
+            .padding(16.dp, 4.dp),
+        value = viewModel.name,
+        onValueChange = { },
+        readOnly = true,
+        placeholder = { Text(stringResource(R.string.name)) },
+        leadingIcon = { Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Name") },
     )
-    {
-        TopAppBar(
-            title = { Text(stringResource(R.string.app_name)) },
-            actions = {
-                IconButton(onClick = { expandMenu = true }) {
-                    Icon(Icons.Default.MoreVert, "Menu")
-                }
-            }
-        )
-        Menu(viewModel)
-        SignOutAlert(viewModel)
-        DeleteAccountAlert(viewModel)
-    }
+    OutlinedTextField(
+        singleLine = true,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp, 4.dp),
+        value = viewModel.city,
+        onValueChange = {  },
+        readOnly = true,
+        placeholder = { Text(stringResource(R.string.city)) },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.AddLocation,
+                contentDescription = "Email"
+            )
+        },
+    )
 }
 
 @Composable
--- FoodAndArt/app/src/main/java/com/example/foodandart/ui/screens/profile/ProfileViewModel.kt
+++ FoodAndArt/app/src/main/java/com/example/foodandart/ui/screens/profile/ProfileViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.foodandart.ui.screens.profile
 
+import android.net.Uri
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
+import com.example.foodandart.data.firestore.cloud_database.userInfo
+import com.example.foodandart.data.firestore.storage.getUserImage
 import com.example.foodandart.data.models.Theme
 import com.example.foodandart.data.repositories.ThemeRepository
 import com.example.foodandart.service.AccountService
@@ -20,8 +24,13 @@ class ProfileViewModel(
 
     var state by mutableStateOf(Theme.System)
 
+    var name by mutableStateOf("")
+    var city by mutableStateOf("")
+
+    var imageUri by mutableStateOf(Uri.EMPTY)
     init {
         viewModelScope.launch {
+            getUserInfo()
             state = repository.theme.first()
         }
     }
@@ -55,4 +64,21 @@ class ProfileViewModel(
             accountService.deleteAccount()
         }
     }
+
+    private fun getUserInfo() {
+        viewModelScope.launch {
+            val document = userInfo()
+            if (document != null && document.data != null) {
+                    for (data in document.data!!) {
+                        when(data.key.toString()) {
+                            "name" -> name = data.value.toString()
+                            "city" -> city = data.value.toString()
+                        }
+                    }
+            }
+            imageUri = getUserImage()
+            Log.d("Uri", imageUri.path.toString())
+        }
+    }
+
 }
