8732b8401a4644cc4cf3bc6e45fec790a9ece27e
luca <luca02c2@gmail.com>
2024-05-02 11:55:15 +0200

    Change method to get favorites Cards

--- FoodAndArt/app/src/main/java/com/example/foodandart/data/firestore/cloud_database/CloudDatabase.kt
+++ FoodAndArt/app/src/main/java/com/example/foodandart/data/firestore/cloud_database/CloudDatabase.kt
@@ -1,20 +1,20 @@
 package com.example.foodandart.data.firestore.cloud_database
 
 import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.example.foodandart.accountService
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.GeoPoint
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.tasks.await
-
-
 fun getCardsWithFilters(
     restaurants: Boolean,
     museums: Boolean,
     packages: Boolean,
-    position: Boolean
 ): MutableList<Query> {
     val db = Firebase.firestore
 
@@ -40,7 +40,7 @@ fun removeUserFiles() {
     db.collection("users").document(accountService.currentUserId).delete()
 }
 
-fun addUserDocument( name : String, cityGeoPoint : GeoPoint, city : String) {
+fun addUserDocument(name: String, cityGeoPoint: GeoPoint, city: String) {
     val db = Firebase.firestore
     val data = hashMapOf(
         "favorites" to emptyList<String>(),
@@ -52,37 +52,27 @@ fun addUserDocument( name : String, cityGeoPoint : GeoPoint, city : String) {
     db.collection("users").document(accountService.currentUserId).set(data)
 }
 
-suspend fun addRemoveFavoriteCard(cardId : String): MutableList<String> {
+suspend fun addRemoveFavoriteCard(cardId: String) {
     val db = Firebase.firestore
-    val favorites = getFavoritesCardsId()
-    if (favorites.contains(cardId)) {
-        favorites.remove(cardId)
-    } else {
-        favorites.add(cardId)
-    }
-    db.collection("users").document(accountService.currentUserId).update("favorites", favorites)
-    return favorites
-}
-
-suspend fun getFavoritesCardsId(): MutableList<String> {
-    val db = Firebase.firestore
-    val favorites = mutableListOf<String>()
-    db.collection("users").document(accountService.currentUserId).get().addOnSuccessListener {document ->
-        document.data?.forEach { item ->
-            if (item.key == "favorites") {
-                (item.value as? List<*>)?.forEach { fav ->
-                    favorites.add(fav.toString())
-                }
+    db.collection("users").document(accountService.currentUserId).get().addOnSuccessListener {
+        val fav = it.data?.get("favorites") as? MutableList<String>
+        if (fav != null) {
+            if (fav.contains(cardId)) {
+                fav.remove(cardId)
+            } else {
+                fav.add(cardId)
             }
+            db.collection("users").document(accountService.currentUserId)
+                .update("favorites", fav)
         }
     }.await()
-    return favorites
 }
 
-suspend fun getFavoritesCards(): MutableList<DocumentSnapshot> {
+
+
+suspend fun getFavoritesCards(favoritesId : List<String>): MutableList<DocumentSnapshot> {
     val db = Firebase.firestore
     val favoritesCards = mutableListOf<DocumentSnapshot>()
-    val favoritesId = getFavoritesCardsId()
     for (id in favoritesId) {
         try {
             val document = db.collection("cards").document(id).get().await()
@@ -90,12 +80,16 @@ suspend fun getFavoritesCards(): MutableList<DocumentSnapshot> {
         } catch (_: Exception) {
         }
     }
-    Log.d("Cards", "FInal size ${favoritesCards.size}")
     return favoritesCards
 }
 
 suspend fun userInfo(): DocumentSnapshot? {
     val db = Firebase.firestore
     return db.collection("users").document(accountService.currentUserId).get().await()
+}
+
+suspend fun getCardById(cardId: String): DocumentSnapshot? {
+    val db = Firebase.firestore
+    return db.collection("cards").document(cardId).get().await()
 }
 
